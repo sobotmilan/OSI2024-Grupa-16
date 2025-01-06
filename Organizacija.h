e81c9b30f539b3b23b3eac9533b41d4e135dc74d
sanjadivljak <divljaksanja12@gmail.com>
2025-01-06 09:53:11 +0100

    Dodana funkcionalnost za unos ključa

--- Organizacija.h
+++ Organizacija.h
@@ -1,15 +1,25 @@
 #pragma once
 #include <iostream>
   
-class Organizacija
-{
+class Organizacija {
 private:
-std::string kljuc;
-std::string nazivOrganizacije;
+    std::string kljuc;
+    std::string nazivOrganizacije;
 public:
-void setKljuc(std::string kljuc){this->kljuc=kljuc;}
- void setNazivOrganizacije(std::string nazivOrganizacije){this->nazivOrganizacije=nazivOrganizacije;}
+    void setKljuc(std::string kljuc){this->kljuc=kljuc;}
+    void setNazivOrganizacije(std::string nazivOrganizacije){this->nazivOrganizacije=nazivOrganizacije;}
 
- std::string getKljuc() const{return kljuc;}
-  std::string getNazivOrganizacije() const{return nazivOrganizacije;}
+    std::string getKljuc() const{return kljuc;}
+    std::string getNazivOrganizacije() const{return nazivOrganizacije;}
+
+    // Metoda za unos ključa
+    bool unosKljuča() {
+        std::string unos;
+        std::cout << "Unesite ključ za aktivaciju komercijalne verzije: ";
+        std::cin >> unos;
+        setKljuc(unos);
+        std::cout << "Ključ je unesen: " << unos << std::endl;
+        return true;
+    }
+
 };
